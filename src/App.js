5f6216d189eac68d8e9d9008452d0e4bc5ec93a8
Alan <foxy.studios.br@gmail.com>
2023-04-13 11:11:34 -0300

    2 components React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { HomePage } from './pages/HomePage'
+import { Test } from './pages/Test'
+
+function App() {
+  return (
+  <>
+    <HomePage></HomePage>
+    <Test></Test>
+  </>
+  )
+}
+
+export default App;
